0dca668f57a3357eaa4d96ed3d8a6ef698084537
ashok948 <ashokskv1@gmail.com>
2025-08-08 12:36:12 +0530

    1

--- app.py
+++ app.py
@@ -1,41 +1,99 @@
-from flask import Flask, render_template
-from flask_mysqldb import MySQL 
-app = Flask(__name__)
-@app.errorhandler(404)
-def page_not_found(e):
-    return "Oops! That page does not exist.", 404
-@app.route('/')
-def home():
-    return "Welcome to the homepage!"
+from flask import Flask, request  
+from flask_mysqldb import MySQL  
+
+app = Flask(__name__)  
+
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'roottoor'
-app.config['MYSQL_DB'] = 'lion_ece'
+app.config['MYSQL_DB'] = 'aug_cse'
 
-mysql = MySQL(app)  
+mysql = MySQL(app)
 
-@app.route("/index", methods=["GET"])
-def index():
-    sql = "SELECT * FROM PEOPLE"  
+@app.route('/')
+def home():
+    return "Hello from Flask!"
+
+@app.route('/save_blog', methods=['POST'])
+def save_blog():
+    #title = request.form["title"]
+    #content = request.form["content"]
+    json =request.get_json()
+    title=json.get("title")
+    content=json.get("content")
     cur = mysql.connection.cursor()
-    cur.execute(sql)  
-    results = cur.fetchall()
+    sql = "insert into blog (title, content) values (%s, %s)"
+    val = [title, content]
+    cur.execute(sql, val)
+    mysql.connection.commit()
     cur.close()
-    return render_template("index.html", results=results)
+    return "Register success"
 
-@app.route("/about", methods=["GET"])
-def about():
-    return render_template("about.html")
 
-@app.route("/blog", methods=["GET"])
-def blogt():
-    return render_template("blog.html")
 
-@app.route("/contact", methods=["GET"])
-def contact():
-    return render_template("contact.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route('/update_blog', methods=['POST'])
+def update_blog():
+    #title = request.form["title"]
+    #content = request.form["content"]
+    json =request.get_json()
+    id=json.get("id")
+    title=json.get("title")
+    content=json.get("content")
+    cur = mysql.connection.cursor()
+    sql = "update blog set title=%s,content=%s where id=%s"
+    val = [title, content,id]
+    cur.execute(sql, val)
+    mysql.connection.commit()
+    cur.close()
+    return "Register success"
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+@app.route('/delete_blog', methods=['POST'])
+def delete_blog():
+    #title = request.form["title"]
+    #content = request.form["content"]
+    json =request.get_json()
+    id=json.get("id")
+    cur = mysql.connection.cursor()
+    sql = "delete from blog where id=%s"
+    val = [id]
+    cur.execute(sql, val)
+    mysql.connection.commit()
+    cur.close()
+    return "Register success"
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+@app.route('/select_blog', methods=['POST'])
+def select_blog():
+    #title = request.form["title"]
+    #content = request.form["content"]
+    json =request.get_json()
+    id=json.get("id")
+    cur = mysql.connection.cursor()
+    sql = "select * from blog where id=%s"
+    val = [id]
+    cur.execute(sql, val)
+    mysql.connection.commit()
+    cur.close()
+    return "Register success"
+
+
+if __name__ == '__main__':
+    app.run()
 
 
